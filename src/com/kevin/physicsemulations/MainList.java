726eff114af853794b1548718dd60c61e532d9d7
kevinwylder <wylderkevin@gmail.com>
2013-07-02 20:41:58 -0700

    Update Main Activity

--- src/com/kevin/physicsemulations/MainList.java
+++ src/com/kevin/physicsemulations/MainList.java
@@ -4,19 +4,20 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
+import android.widget.*;
 
 public class MainList extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		Intent intent=new Intent(this, ViewActivity.class);
+		ListView lv=new ListView(this);
+		lv.setAdapter(Adapter);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+		// Inflate the menu; this adds items to the action bar if it is present
 		getMenuInflater().inflate(R.menu.main_list, menu);
 		return true;
 	}
