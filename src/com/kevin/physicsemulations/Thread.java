a94a3e327a148980f7e088aa2f28d72fea296ae4
kevinwylder <wylderkevin@gmail.com>
2013-07-06 09:30:48 -0700

    Change working in Thread.java

--- src/com/kevin/physicsemulations/Thread.java
+++ src/com/kevin/physicsemulations/Thread.java
@@ -13,11 +13,14 @@ public class Thread extends BaseView
 	ArrayList<PointF> points=new ArrayList<PointF>();
 	ArrayList<PointF> velocities=new ArrayList<PointF>();
 	ArrayList<PointF> forces=new ArrayList<PointF>();
+	PointF finger=new PointF(-1,-1);
 	float gap;
-	float k=.0005f;
-	float dt=.4f;
+	float k=.01f;
+	float dt=.1f;
 	boolean gravity=false;
 	int grabbedIndex=0;
+	float maxForce=0;
+	float minForce=0;
 	
 	public Thread(Context ctx){
 		super(ctx);
@@ -29,52 +32,37 @@ public class Thread extends BaseView
 	}
 
 	public void drawCanvas(Canvas canvas){
+		if(finger.x!=-1&finger.y!=-1)points.get(grabbedIndex).set(finger);
 		for(int i=0;i<forces.size();i++){
 			forces.get(i).set(0,0);
-		}
-		for(int A=0;A<1;A++){
-			for(int i=grabbedIndex+1;i<points.size();i++){
-				PointF parent=points.get(i-1);
-				PointF self=points.get(i);
-				PointF child=new PointF();
-				try{
-					child=points.get(i+1);
-				}catch(Exception e){
-					child.set(self);
-				}
-				double distanceParent=Math.sqrt(Math.pow(parent.x-self.x,2)+Math.pow(parent.y-self.y,2));
-				double distanceChild=Math.sqrt(Math.pow(child.x-self.x,2)+Math.pow(child.y-self.y,2));
-				if(distanceParent>gap){
-					double angle=Math.atan2(parent.y-self.y,parent.x-self.x);
-					forces.get(i).x+=(float)(Math.pow((distanceParent-gap),2)*Math.cos(angle))*k;
-					forces.get(i).y+=(float)(Math.pow((distanceParent-gap),2)*Math.sin(angle))*k;
-				}else if(distanceChild>gap){
-					double angle=Math.atan2(self.y-child.y,self.x-child.x);
-					forces.get(i).x+=(float)(Math.pow((distanceChild-gap),2)*Math.cos(angle))*k;
-					forces.get(i).y+=(float)(Math.pow((distanceChild-gap),2)*Math.sin(angle))*k;
-				}
-			}	
-			for(int i=grabbedIndex-1;i>=0;i--){
-				PointF parent=points.get(i+1);
-				PointF self=points.get(i);
-				PointF child=new PointF();
-				try{
-					child=points.get(i-1);
-				}catch(Exception e){
-					child.set(self);
-				}
-				double distanceParent=Math.sqrt(Math.pow(parent.x-self.x,2)+Math.pow(parent.y-self.y,2));
-				double distanceChild=Math.sqrt(Math.pow(child.x-self.x,2)+Math.pow(child.y-self.y,2));
-				if(distanceParent>gap){
-					double angle=Math.atan2(parent.y-self.y,parent.x-self.x);
-					forces.get(i).x+=(float)(Math.pow((distanceParent-gap),2)*Math.cos(angle))*k;
-					forces.get(i).y+=(float)(Math.pow((distanceParent-gap),2)*Math.sin(angle))*k;
-				}else if(distanceChild>gap){
-					double angle=Math.atan2(self.y-child.y,self.x-child.x);
-					forces.get(i).x+=(float)(Math.pow((distanceChild-gap),2)*Math.cos(angle))*k;
-					forces.get(i).y+=(float)(Math.pow((distanceChild-gap),2)*Math.sin(angle))*k;
-				}
+		/*	PointF self=points.get(i);
+			PointF parent;
+			PointF child;
+			try{
+				parent=points.get(i-1);
+			}catch(Exception e){
+				parent=self;
+			}try{
+				child=points.get(i+1);
+			}catch(Exception e){
+				child=self;
 			}
+			double dParent=distance(self,parent);
+			double dChild=distance(self,child);
+			if(dParent>gap*4){
+				double angle=Math.atan2(parent.y-self.y,parent.x-self.x);
+				points.get(i-1).set(self.x+(float)Math.cos(angle)*gap*2,self.y+(float)Math.sin(angle)*gap*2);
+			}
+			if(dChild>gap*4){
+				double angle=Math.atan2(child.y-self.y,child.x-self.x);
+				points.get(i+1).set(self.x+(float)Math.cos(angle)*gap*2,self.y+(float)Math.sin(angle)*gap*2);
+			}*/
+		}
+		for(int i=grabbedIndex;i<points.size();i++){
+			setForces(i,false);
+		}
+		for(int i=grabbedIndex;i>0;i--){
+			setForces(i,true);
 		}
 		for(int i=0;i<points.size();i++){
 			float mass=.5f;
@@ -82,8 +70,8 @@ public class Thread extends BaseView
 			velocities.get(i).y+=(forces.get(i).y*dt)/mass;
 			points.get(i).x+=velocities.get(i).x*dt;
 			points.get(i).y+=velocities.get(i).y*dt;
-			velocities.get(i).x*=.95f;
-			velocities.get(i).y*=.95f;
+			velocities.get(i).x*=.8f;
+			velocities.get(i).y*=.8f;
 		}
 		for(int i=1;i<points.size();i++){
 			canvas.drawLine(points.get(i-1).x,points.get(i-1).y,points.get(i).x,points.get(i).y,paint);
@@ -92,7 +80,8 @@ public class Thread extends BaseView
 
 	public void handleTouch(MotionEvent event){
 		if(event.getAction()==MotionEvent.ACTION_MOVE){
-			points.get(grabbedIndex).set(event.getX(),event.getY());
+		//	points.get(grabbedIndex).set(event.getX(),event.getY());
+		    finger.set(event.getX(),event.getY());
 		}else if(event.getAction()==MotionEvent.ACTION_DOWN){
 			double closest=Math.pow(2,20);
 			for(int i=0;i<points.size();i++){
@@ -102,6 +91,8 @@ public class Thread extends BaseView
 					grabbedIndex=i;
 				}
 			}
+		}else if(event.getAction()==MotionEvent.ACTION_UP){
+			finger.set(-1,-1);
 		}
 	}
 
@@ -119,25 +110,44 @@ public class Thread extends BaseView
 		}
 	}
 	
-	public void setForces(int index){
-		PointF home=points.get(index);
-		for(int i=0;i<points.size();i++){
-			if(i!=index){
+	public void setForces(int index,boolean up){
+		if(!up){
+			PointF home=points.get(index);
+			for(int i=index+1;i<points.size();i++){
 				PointF guest=points.get(i);
-				float dT=Math.abs(index-i)*gap;
-				float dA=(float)Math.sqrt(Math.pow(home.x-guest.x,2)+Math.pow(home.y-guest.y,2));
-				if(dT>dA){
-					double angle;
-					if(i<index){
-						angle=Math.atan2(guest.y-home.y,guest.x-home.x);
-					}else{
-						angle=Math.atan2(home.y-guest.y,home.x-guest.x);
-					}
-					forces.get(i).x+=(float)(Math.pow((dA-dT),2)*Math.cos(angle))*k;
-					forces.get(i).y+=(float)(Math.pow((dA-dT),2)*Math.sin(angle))*k;
-				}
+				float dT=(i-index)*gap;
+				float dA=(float)distance(home,guest);
+				if(dT<dA){
+					double angle=Math.atan2(guest.y-home.y,guest.x-home.x)+Math.PI;
+					forces.get(i).x+=(float)(Math.pow((dA-dT),2)*Math.cos(angle))*k/((i-index)*.2f);
+					forces.get(i).y+=(float)(Math.pow((dA-dT),2)*Math.sin(angle))*k/((i-index)*.2f);
+				}/*else if(dT-dA<gap*.3*(index-i)){
+					double angle=Math.atan2(guest.y-home.y,guest.x-home.x)+Math.PI;
+					forces.get(i).x+=(float)(-Math.pow((dA-dT),2)*Math.cos(angle))*k;//((i-index)*.5f);
+					forces.get(i).y+=(float)(-Math.pow((dA-dT),2)*Math.sin(angle))*k;//((i-index)*.5f);
+				}*/
+			}
+		}else{
+			PointF home=points.get(index);
+			for(int i=index-1;i>=0;i--){
+				PointF guest=points.get(i);
+				float dT=(index-i)*gap;
+				float dA=(float)distance(home,guest);
+				if(dT<dA){
+					double angle=Math.atan2(guest.y-home.y,guest.x-home.x)+Math.PI;
+					forces.get(i).x+=(float)(Math.pow((dA-dT),2)*Math.cos(angle))*k/((index-i)*.2f);
+					forces.get(i).y+=(float)(Math.pow((dA-dT),2)*Math.sin(angle))*k/((index-i)*.2f);
+				}/*else if(dT-dA<gap*.3*(index-i)){
+					double angle=Math.atan2(guest.y-home.y,guest.x-home.x)+Math.PI;
+					forces.get(i).x+=(float)(-Math.pow((dA-dT),2)*Math.cos(angle))*k;//((i-index)*.5f);
+					forces.get(i).y+=(float)(-Math.pow((dA-dT),2)*Math.sin(angle))*k;//((i-index)*.5f);
+				}*/
 			}
 		}
+	}
+	
+	public double distance(PointF a, PointF b){
+		return Math.sqrt(Math.pow(a.x-b.x,2)+Math.pow(a.y-b.y,2));
 	}
 	
 }
